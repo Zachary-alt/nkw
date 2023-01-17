6c6b42f0633a297437dc9b7ceb16277d73ba0a05
zhaozz <842122020@qq.com>
2023-01-17 16:48:51 +0800

    update

--- 108最小公倍数.js
+++ 108最小公倍数.js
@@ -1,8 +1,8 @@
 // 最大公约数
 function gcd(a,b){
-    if(a===0) return a;
-    let c=b%a
-    return gcd(a,c)
+    if(b===0) return a;
+    let c=a%b
+    return gcd(b,c)
 }
 // 最小公倍数
 function scm(a,b){
